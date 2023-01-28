b42ecfce3500e03d18511f960c5e5063e0924e14
GatekoNeko141 <jhon1995jera@gmail.com>
2023-01-28 13:16:31 -0500

    Produccion: -Usuario normal de prueba -Conexion a base de datos remota

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
 const mysql = require("mysql")
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_DATABASE
-})
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_DATABASE
+// })
+
+const db = mysql.createConnection("mysql://root:jKtqlxfcW9Yr8g5Z6cUL@containers-us-west-168.railway.app:7011/railway")
 
 db.connect(err => {
   if(err){
